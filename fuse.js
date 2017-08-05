32080aa2733d5a9b33c60721d413cd7eb3ea98c1
Roman Heinrich <roman.heinrich@gmail.com>
2017-08-05 21:06:32 +0200

    playing around with sparky

--- fuse.js
+++ fuse.js
@@ -1,39 +1,89 @@
-const { FuseBox } = require("fuse-box");
-const { SassPlugin, CSSPlugin } = require("fuse-box");
+// const { FuseBox } = require("fuse-box");
+// const { SassPlugin, CSSPlugin } = require("fuse-box");
+// const { Sparky } = require("fuse-box");
 
-let tmp = "./tmp";
-const fuse = FuseBox.init({
-  homeDir: ".",
-  output: "dist/$name.js",
-  sourceMaps: true,
-  plugins: [
-    [
+// // let fuse, app, isProduction;
+// let tmp = "./tmp";
+// const fuse = FuseBox.init({
+//   homeDir: ".",
+//   output: "dist/$name.js",
+//   sourceMaps: true,
+//   plugins: [
+//     [
+//       SassPlugin(),
+//       CSSPlugin({
+//         outFile: file => `dist/${file}`
+//       })
+//     ]
+//   ]
+// });
+
+// fuse.bundle("a").instructions(`>spectre.scss`);
+// fuse.bundle("a").instructions(`>spectre-exp.scss`);
+// fuse.bundle("a").instructions(`>spectre-icons.scss`);
+// fuse.run();
+
+// // disable sourcemaps and enable minification
+// fuse.opts.sourceMaps = false;
+// fuse.opts.plugins = [
+//   [
+//     SassPlugin(),
+//     CSSPlugin({
+//       outFile: file => `dist/${file.slice(0, -4)}.min.css`,
+//       minify: true
+//     })
+//   ]
+// ];
+
+// fuse.bundle("a").instructions(`>spectre.scss`);
+// fuse.bundle("a").instructions(`>spectre-exp.scss`);
+// fuse.bundle("a").instructions(`>spectre-icons.scss`);
+// fuse.run();
+
+const { FuseBox, Sparky, SassPlugin, CSSPlugin } = require("fuse-box");
+
+let fuse, app, isProduction;
+
+Sparky.task("config", () => {
+  let plugins = [];
+  if (isProduction) {
+    plugins.push([
       SassPlugin(),
       CSSPlugin({
-        outFile: file => `dist/${file}`
+        outFile: file => `dist/${file.slice(0, -4)}.min.css`,
+        minify: true
       })
-    ]
-  ]
+    ]);
+  }
+  if (!isProduction) {
+    plugins.push([
+      SassPlugin(),
+      CSSPlugin({
+        outFile: file => `dist/${file}`,
+        minify: false
+      })
+    ]);
+  }
+  // debugger;
+
+  fuse = FuseBox.init({
+    experimentalFeatures: true,
+    homeDir: ".",
+    output: "dist/$name.js",
+    hash: isProduction,
+    plugins: plugins
+  });
+  app = fuse.bundle("a").watch("src/**").instructions(`>spectre.scss`);
 });
 
-fuse.bundle("a").instructions(`>spectre.scss`);
-fuse.bundle("a").instructions(`>spectre-exp.scss`);
-fuse.bundle("a").instructions(`>spectre-icons.scss`);
-fuse.run();
+Sparky.task("clean", () => {
+  return Sparky.src("dist/").clean("dist/");
+});
 
-// disable sourcemaps and enable minification
-fuse.opts.sourceMaps = false;
-fuse.opts.plugins = [
-  [
-    SassPlugin(),
-    CSSPlugin({
-      outFile: file => `dist/${file.slice(0, -4)}.min.css`,
-      minify: true
-    })
-  ]
-];
-
-fuse.bundle("a").instructions(`>spectre.scss`);
-fuse.bundle("a").instructions(`>spectre-exp.scss`);
-fuse.bundle("a").instructions(`>spectre-icons.scss`);
-fuse.run();
+Sparky.task("default", ["clean", "config"], () => fuse.run());
+Sparky.task("set-production", () => {
+  isProduction = true;
+});
+Sparky.task("dist", ["clean", "set-production", "config"], () => {
+  fuse.run();
+});
