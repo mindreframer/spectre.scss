d03323d398530e41a7075902454abb6d2d491fc2
Roman Heinrich <roman.heinrich@gmail.com>
2017-08-05 19:25:27 +0200

    yarn.lock + fix for watch + gulp / gulp-watch npm commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer')
-var cleancss = require('gulp-clean-css');
-var csscomb = require('gulp-csscomb');
-var rename = require('gulp-rename');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var cleancss = require("gulp-clean-css");
+var csscomb = require("gulp-csscomb");
+var rename = require("gulp-rename");
+var gutil = require("gulp-util");
+var plumber = require("gulp-plumber");
 
-var autoprefix = autoprefixer({ browsers: ['last 3 versions'] });
+var autoprefix = autoprefixer({ browsers: ["last 3 versions"] });
 
-gulp.task('watch', function() {
-  gulp.watch('./**/*.scss', ['build']);
+gulp.task("watch", function() {
+  gulp.watch("./**/*.scss", ["build"]);
 });
 
-gulp.task('build', function() {
-  gulp.src('./*.scss')
-    .pipe(sass().on('error', sass.logError))
+gulp.task("build", function() {
+  gulp
+    .src("./*.scss")
+    .pipe(
+      plumber(function(error) {
+        gutil.log(error.message);
+        this.emit("end");
+      })
+    )
+    .pipe(sass())
     .pipe(autoprefix)
     .pipe(csscomb())
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest("./dist"))
     .pipe(cleancss())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(
+      rename({
+        suffix: ".min"
+      })
+    )
+    .pipe(gulp.dest("./dist"));
 });
 
-gulp.task('default', ['build']);
+gulp.task("default", ["build"]);
